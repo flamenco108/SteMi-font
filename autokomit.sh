ea0a53b200aafd75fd737fc6cd2f93a1694fa759
Krzysztof Stenografow <flamenco108@gmail.com>
2023-07-26 09:06:29 +0200

    flamenco AUTO-KOMIT śro, 26 lip 2023, 09:06:29 CEST

--- autokomit.sh
+++ autokomit.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-komitmessage="$USER@$(hostname -s) robi auto-komit z dnia $(date)" #auto wiado dla komita
+#komitmessage="$USER@$(hostname -s) robi auto-komit z dnia $(date)" #auto wiado dla komita
+komitmessage="$USER AUTO-KOMIT $(date)" #auto wiado dla komita
 GIT=`which git` #pełna ścieżka do git
 # komitujemy
 ${GIT} add --all . && \
