00c6efe54006e793410a717df925850ab11c55ec
Krzysztof Stenografow <flamenco108@gmail.com>
2024-07-12 12:02:11 +0200

    flamenco@szmirpal AUTO-KOMIT pią, 12 lip 2024, 12:01:42 CEST

--- autokomit.sh
+++ autokomit.sh
@@ -1,10 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-#komitmessage="$USER@$(hostname -s) robi auto-komit z dnia $(date)" #auto wiado dla komita
-komitmessage="$USER@$(hostname -s) AUTO-KOMIT $(date)" #auto wiado dla komita
-#komitmessage="$USER AUTO-KOMIT $(date)" #auto wiado dla komita
-GIT=`which git` #pełna ścieżka do git
-# komitujemy
-${GIT} add --all . && \
-${GIT} commit -m "$komitmessage" && \
-${GIT} push origin theory-dev
+# Domyślna wiadomość komitu
+default_message="$USER@$(hostname -s) AUTO-KOMIT $(date)"
+
+# Funkcja do odliczania czasu
+countdown() {
+    local seconds=$1
+    while [ $seconds -gt 0 ]; do
+        echo -ne "\rCzas na wprowadzenie wiadomości: $seconds s "
+        sleep 1
+        : $((seconds--))
+    done
+    echo
+}
+
+# Prośba o wprowadzenie wiadomości komitu
+echo "Wprowadź wiadomość komitu (lub naciśnij Enter dla domyślnej wiadomości):"
+echo "Domyślna wiadomość: $default_message"
+
+# Uruchomienie odliczania w tle
+countdown 30 &
+countdown_pid=$!
+
+# Oczekiwanie na input użytkownika z timeoutem
+read -t 30 komitmessage
+
+# Zatrzymanie procesu odliczania
+kill $countdown_pid 2>/dev/null
+
+# Jeśli użytkownik nie wprowadził niczego, użyj domyślnej wiadomości
+if [ -z "$komitmessage" ]; then
+    komitmessage="$default_message"
+fi
+
+GIT=$(which git)
+
+# Komitowanie zmian
+$GIT add --all . && \
+$GIT commit -m "$komitmessage" && \
+$GIT push origin theory-dev
